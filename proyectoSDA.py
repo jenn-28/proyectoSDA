2698135917457f839bb82c2f8f55f709e1409bc0
Jenn <jennicast76@gmail.com>
2026-05-26 21:21:19 -0600

    Segunada etapa

--- proyectoSDA.py
+++ proyectoSDA.py
@@ -1,6 +1,8 @@
 # pylint: disable=no-member
 import pygame
 import collections
+import heapq
+
 
 # Configuración de colores
 BLANCO = (255, 255, 255) #Mapa
@@ -12,11 +14,15 @@ AZUL = (0, 0, 255)    # Explorando BFS
 AMARILLO = (255, 255, 0) # Camino final
 MORADO = (128, 0, 128) # Explorando DFS
 NARANJA = (255, 165, 0) #explorando DLS
+CAFE = (139, 69, 19) # Lodo (costo 5)
+CELESTE = (0, 255, 255)  # Exploración A*
 
 # Dimensiones del mapa
 ANCHO, ALTO = 600, 600
 COLUMNAS, FILAS = 30, 30
 TAMANO_CELDA = ANCHO // COLUMNAS
+
+#PRIMERA ETAPA: BFS, DFS, DLS, IDDFS
 
 def bfs(inicio, fin, muros, ventana):
     conteo = 0
@@ -164,11 +170,94 @@ def iddfs(inicio, fin, muros, ventana):
     print("IDDFS Finalizado. Fuiste devorado por los infectados")
     esperar_reinicio(muros)
     return False
-    
+
+#SEGUNDA ETAPA: A*
+def algoritmo_a(inicio, fin, muros, terrenos, ventana):
+
+    cola = []
+    heapq.heappush(cola, (0, inicio))
+
+    padres = {inicio: None}
+    # g(n)
+    costos = {inicio: 0}
+    nodos_explorados = 0
+
+    while cola:
+
+        prioridad_actual, actual = heapq.heappop(cola)
+        nodos_explorados += 1
+
+        # Llegó al final
+        if actual == fin:
+
+            while actual is not None:
+
+                dibujar_celda(ventana, actual, AMARILLO)
+                dibujar_celda(ventana, inicio, VERDE)
+                dibujar_celda(ventana, fin, ROJO)
+
+                actual = padres[actual]
+
+                pygame.display.update()
+
+            print("A* Finalizado")
+            print("Nodos explorados:", nodos_explorados)
+            print("Costo total:", costos[fin])
+
+            esperar_reinicio(muros)
+
+            return True
+
+        r, c = actual
+
+        # vecinos
+        for dr, dc in [(0,1), (0,-1), (1,0), (-1,0)]:
+
+            vecino = (r + dr, c + dc)
+
+            if 0 <= vecino[0] < FILAS and 0 <= vecino[1] < COLUMNAS:
+
+                # ignorar muros
+                if vecino in muros:
+                    continue
+
+                # costo del terreno
+                costo_terreno = terrenos.get(vecino, 1)
+
+                # g(n)
+                nuevo_costo = costos[actual] + costo_terreno
+
+                # si encontramos camino más barato
+                if vecino not in costos or nuevo_costo < costos[vecino]:
+
+                    costos[vecino] = nuevo_costo
+
+                    # h(n)
+                    heuristica = abs(fin[0] - vecino[0]) + abs(fin[1] - vecino[1])
+
+                    # f(n)
+                    prioridad = nuevo_costo + heuristica
+
+                    heapq.heappush(cola, (prioridad, vecino))
+
+                    padres[vecino] = actual
+
+                    if vecino != fin:
+                        dibujar_celda(ventana, vecino, CELESTE)
+
+        pygame.display.update()
+        pygame.time.delay(20)
+
+    print("A* Finalizado. No hay camino.")
+    esperar_reinicio(muros)
+
+    return False
+
+
 def dibujar_celda(win, pos, color):
     pygame.draw.rect(win, color, (pos[1]*TAMANO_CELDA, pos[0]*TAMANO_CELDA, TAMANO_CELDA-1, TAMANO_CELDA-1))
 
-def esperar_reinicio(muros):
+def esperar_reinicio(muros, terrenos):
     esperando = True
     while esperando:
         for evento in pygame.event.get():
@@ -180,6 +269,7 @@ def esperar_reinicio(muros):
                     esperando = False
                 elif evento.key == pygame.K_l:
                     muros.clear()
+                    terrenos.clear()
                     esperando = False
 
 def main():
@@ -190,6 +280,7 @@ def main():
     inicio = None
     fin = None
     muros = set()
+    terrenos = {}
     corriendo = True
 
     etapa = 0
@@ -202,6 +293,7 @@ def main():
                 dibujar_celda(ventana, (r, c), BLANCO)
 
         for m in muros: dibujar_celda(ventana, m, NEGRO)
+        for t in terrenos: dibujar_celda(ventana, t, CAFE)
         if inicio: dibujar_celda(ventana, inicio, VERDE)
         if fin: dibujar_celda(ventana, fin, ROJO)
 
@@ -239,10 +331,11 @@ def main():
                 elif evento.key == pygame.K_x:
                     etapa = 0
                     muros.clear() 
+                    terrenos.clear()
                     inicio=None
                     fin=None     
                     pygame.display.set_caption("Simulador de Grafos")
-
+#ETAPA 1: BFS, DFS, DLS, IDDFS
                 elif etapa == 1 and algoritmo == "":
                     if evento.key == pygame.K_b: 
                         algoritmo = "BFS"
@@ -280,6 +373,39 @@ def main():
                         else:
                             print("Debes colocar el INICIO y el FIN en el mapa antes de iniciar.")
 
+ #ETAPA 2: A*, BFS              
+                elif etapa == 2 and algoritmo == "":
+                    if evento.key == pygame.K_a:
+                        algoritmo = "A*"
+                        pygame.display.set_caption("Simulador de Grafos: Etapa 2 - A*")
+                    elif evento.key == pygame.K_b:
+                        algoritmo = "BFS"
+                        pygame.display.set_caption("Simulador de Grafos: Etapa 2 - BFS")
+                    elif evento.key == pygame.K_t:
+                        etapa = 0
+                        muros.clear() 
+                        terrenos.clear()
+                        inicio=None
+                        fin=None                       
+                        pygame.display.set_caption("Simulador de Grafos")    
+
+                elif etapa == 2 and algoritmo != "":
+                    if evento.key == pygame.K_SPACE:
+                        if inicio is not None and fin is not None:
+                            if algoritmo == "BFS":
+                                bfs(inicio, fin, muros, ventana)
+                            if algoritmo == "A*":
+                                algoritmo_a(inicio, fin, muros, terrenos, ventana)
+                            algoritmo = ""
+                            pygame.display.set_caption("Simulador de Grafos: Etapa 2")
+                        else:
+                            print("Debes colocar el INICIO y el FIN en el mapa antes de iniciar.")
+
+#ETAPA 3
+                elif etapa == 3 and algoritmo == "":
+                    print("En construccion...")
+                    etapa = 0
+
             if etapa != 0 and inicio is not None and fin is not None:
                 if pygame.mouse.get_pressed()[0]:
                     pos = pygame.mouse.get_pos()
@@ -289,6 +415,17 @@ def main():
 
                     if posicion != inicio and posicion != fin:
                         muros.add(posicion)
+
+                if etapa == 2 and pygame.mouse.get_pressed()[2]:
+                    pos = pygame.mouse.get_pos()
+                    fila = pos[1]//TAMANO_CELDA
+                    col = pos[0]//TAMANO_CELDA
+                    posicion = (fila, col)
+
+                    if posicion != inicio and posicion != fin  and posicion not in muros:
+                        terrenos[posicion] = 5
+
+                    
 
         pygame.display.update()
 
